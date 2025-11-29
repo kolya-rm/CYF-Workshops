1103cc41557618888669217f5451cac3e9beb366
Nikolai Romanov <kolya.rm@gmail.com>
2025-11-29 12:50:34 +0100

    Added solution for level-500.js.

--- js1-wk1-eval/level-500.js
+++ js1-wk1-eval/level-500.js
@@ -5,3 +5,5 @@ const avgDaysInMonth = Math.floor(365 / 12);
 // Without running any code,
 // Predict and explain what the value of avgDaysInMonth will evaluate to
 // You can use documentation to look up what Math.floor does
+
+// 30 
