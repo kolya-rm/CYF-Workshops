6093eb7674609bb1a0637c2cf6bbfaaa6053af4f
Nikolai Romanov <kolya.rm@gmail.com>
2025-11-29 15:33:06 +0100

    Addes solution for level-900.js.

--- js1-wk1-eval/level-900.js
+++ js1-wk1-eval/level-900.js
@@ -6,3 +6,7 @@ const grandFatherAge = 83;
 
 // Create a variable called ageGap and assign to it the difference between grandFatherAge and mitchAge
 // Log the value of ageGap to the console 
+
+let ageGap = grandFatherAge - mitchAge;
+
+console.log(ageGap);
